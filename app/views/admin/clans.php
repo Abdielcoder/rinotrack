3317346568ba9bfd3001c92faad424a5782acb0e
Abdielcoder <redskullcoder@gmail.com>
2025-08-13 08:31:22 -0700

    Input Height

--- app/views/admin/clans.php
+++ app/views/admin/clans.php
@@ -234,7 +234,7 @@ $additionalCSS = [];
                 <label for="userSelect">Agregar Usuario</label>
                 <div class="user-picker">
                     <input type="text" id="userSearch" placeholder="Buscar por nombre, usuario o email" />
-                    <select id="userSelect">
+                    <select id="userSelect" style="height: 44px;">
                         <option value="">Seleccionar usuario</option>
                     </select>
                 </div>
