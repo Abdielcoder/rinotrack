ea9722a80c48cfe780ccfb82fda2b2d3043a85a4
Abdielcoder <redskullcoder@gmail.com>
2025-08-13 08:35:16 -0700

    Input Height-5

--- app/views/admin/clans.php
+++ app/views/admin/clans.php
@@ -229,7 +229,7 @@ $additionalCSS = [];
             </button>
         </div>
         
-        <div class="modal-body" style="width: 100%;">
+        <div class="modal-body" style="width: 500px; !important">
             <div class="form-group">
                 <label for="userSelect">Agregar Usuario</label>
                 <div class="user-picker">
