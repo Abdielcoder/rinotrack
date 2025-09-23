c6e47115a67ea3e9a8bf0ea7955c3b2cbc630724
Abdiel C <desarrollo@rinorisk.com>
2025-09-22 20:58:44 -0700

    Avatares-5

--- app/views/clan_leader/members.php
+++ app/views/clan_leader/members.php
@@ -959,8 +959,8 @@ document.addEventListener('DOMContentLoaded', function() {
 }
 
 .dropdown-avatar-image {
-    width: calc(100% + 4px);
-    height: calc(100% + 4px);
+    width: calc(100% + 14px);
+    height: calc(100% + 14px);
     object-fit: cover;
     border-radius: 50%;
     display: block;
