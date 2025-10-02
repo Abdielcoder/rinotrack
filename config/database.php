c3bbe292fd5bf76de0d006582bfdf1dc81c5e692
Abdiel C <desarrollo@rinorisk.com>
2025-10-02 15:20:08 -0700

    Cambio de Rama produccionBeta2

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '/VFwtcC6Xj18'); // Cambia esto por tu contraseña de MySQL en MAMP
 define('DB_NAME', 'rinotrack');
-
+//Otro com
 /**
  * Clase para manejar la conexión a la base de datos
  */
